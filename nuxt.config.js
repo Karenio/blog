90b09953fb470d32cd1cb5538490728160bf07fd
karenio <furutakaren141@gmail.com>
2021-04-25 17:41:35 +0900

    add:@nuxt/content

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/content'
   ],
+  content: {
+      // Options
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
