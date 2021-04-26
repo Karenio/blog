0a1d202c00b4df7ae6634646d9cc888bb42349b2
karenio <furutakaren141@gmail.com>
2021-04-26 11:01:51 +0900

    add:introduction

--- pages/blogs/_slug.vue
+++ pages/blogs/_slug.vue
@@ -1,10 +1,25 @@
 <template>
- <article>
-   <h1>{{ blogs.title }}</h1>
-   <p>{{ blogs.date }}</p>
-   <nuxt-content :document="blogs" />
- </article>
+  <v-row justify="center" align="center" class="background">
+    <v-col cols="12" sm="10" md="9" class="col-center">
+      <article>
+        <div class="py-3">
+          {{ blogs.date }}
+          <h1>{{ blogs.title }}</h1>
+          <div class="pb-3">
+          <!--<tag-tip
+            v-for="tag in article.tags"
+            :key="tag"
+            class="my-1 mr-2"
+            :tag="tag"
+          ></tag-tip>-->
+          </div>
+          <nuxt-content :document="blogs" />
+        </div>
+      </article>
+    </v-col>
+  </v-row>
 </template>
+
 <script>
 export default {
  async asyncData ({ $content, params }) {
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,14 +1,20 @@
 <template>
  <div>
    <div v-for="b in blogs" :key="b.slug">
-       <v-card class="mx-auto my-12" max-width="800">
-            <v-card-title class="white">
-                <nuxt-link :to="'/blogs/'+ b.slug">{{b.title}} {{b.date}}</nuxt-link>
+       <v-card class="mx-auto my-12 white" max-width="800">
+           <v-card-text>
+           <nuxt-link :to="'/blogs/'+ b.slug">{{ b.date }}</nuxt-link>
+           </v-card-text>
+            <v-card-title>
+                <nuxt-link :to="'/blogs/'+ b.slug">
+                    {{ b.title }}
+                </nuxt-link>
             </v-card-title>
        </v-card>
    </div>
  </div>
 </template>
+
 <script>
 export default {
  async asyncData ({ $content, params }) {
