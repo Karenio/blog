a1c1cfd01bd7434184dce7d56b5cf1cc73cea5c5
karenio <furutakaren141@gmail.com>
2021-04-29 14:49:41 +0900

    setting: title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - blog',
-    title: 'blog',
+    titleTemplate: 'rinruren',
+    title: 'rinruren',
     htmlAttrs: {
       lang: 'en'
     },
